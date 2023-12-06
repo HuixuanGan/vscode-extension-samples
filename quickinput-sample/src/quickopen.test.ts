4eaed32758304767fe5801e0565edf22a47e056e
HuixuanGan <huixuang@umich.edu>
2023-12-05 20:55:42 -0800

    add test

--- quickinput-sample/src/quickopen.test.ts
+++ quickinput-sample/src/quickopen.test.ts
@@ -1,19 +1,33 @@
 import * as vscode from 'vscode';
-import { quickOpen} from './quickOpen';
+import { quickOpen } from './quickOpen';
 
 jest.mock('vscode', () => ({
-	workspace: {
-      openTextDocument: jest.fn().mockResolvedValue({}),
-	},
-	window: {
-      showTextDocument: jest.fn(),
-	},
-  }));
-  
-  describe('quickOpen function', () => {
-	it('calls openTextDocument and showTextDocument', async () => {
-      await quickOpen();
-      expect(vscode.workspace.openTextDocument).toHaveBeenCalled();
-      expect(vscode.window.showTextDocument).toHaveBeenCalled();
-	});
+  workspace: {
+    openTextDocument: jest.fn().mockResolvedValue({}),
+  },
+  window: {
+    showTextDocument: jest.fn(),
+    createQuickPick: jest.fn().mockReturnValue({
+      onDidChangeValue: jest.fn(),
+      onDidChangeSelection: jest.fn().mockImplementation(callback => {
+        callback([{ label: 'Test File' }]); // Mock QuickPickItem without uri
+      }),
+      onDidHide: jest.fn(),
+      show: jest.fn(),
+      items: [],
+      dispose: jest.fn(),
+    }),
+  },
+  Uri: {
+    parse: jest.fn(),
+  },
+}));
+
+describe('quickOpen function', () => {
+  it('calls openTextDocument and showTextDocument', async () => {
+    await quickOpen();
+
+    expect(vscode.workspace.openTextDocument).toHaveBeenCalled();
+    expect(vscode.window.showTextDocument).toHaveBeenCalled();
   });
+});
